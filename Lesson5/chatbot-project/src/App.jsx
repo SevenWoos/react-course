c0fff8696c2e821e00e6e98e89d0593e31120ea0
SevenWoos <swenwu96@gmail.com>
2026-04-11 14:07:45 -0700

    Lesson 5: Proper React Setup with Vite

---
+++ Lesson5/chatbot-project/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { ChatInput } from './components/ChatInput';
+import ChatMessages from './components/ChatMessages';
+import './App.css'
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([{
+    message: 'hello chatbot', 
+    sender: 'user', 
+    id: crypto.randomUUID()
+  }, {
+    message: 'Hello! How can I help you?', 
+    sender: 'robot', 
+    id: crypto.randomUUID()
+  }, {
+    message: 'can you get me todays date?', 
+    sender: 'user', 
+    id: crypto.randomUUID()
+  }, 
+{
+  message: 'Today is April 11', 
+  sender: 'robot', 
+  id: crypto.randomUUID()
+}]);
+
+  return (
+    <div className="app-container">
+      {chatMessages.length === 0 && (
+        <p className="welcome-message">
+          Welcome to the Chatbot project! Send a message using the textbox below.  
+        </p>
+      )}
+
+      <ChatMessages 
+        chatMessages={chatMessages}
+      />
+
+      <ChatInput 
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  )
+};
+
+export default App
---
+++ Lesson5/chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { Chatbot } from 'supersimpledev';
+import LoadingImage from '.././assets/loading-spinner.gif'
+import './ChatInput.css';
+
+// ChatInput Component
+export function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState('');
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  };
+
+  async function sendMessage() {
+    const newChatMessages = [
+      ...chatMessages, 
+      {
+        message: inputText, 
+        sender: 'user', 
+        id: crypto.randomUUID()
+      }
+    ];
+
+    setChatMessages([
+      ...newChatMessages, 
+      {
+        message: <img src={LoadingImage} className="loading-spinner"/>, 
+        sender: 'robot', 
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    const response = await Chatbot.getResponseAsync(inputText);
+
+    setChatMessages([
+      ...newChatMessages, 
+      {
+        message: response, 
+        sender: 'robot', 
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    setInputText('');
+  };
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      sendMessage();
+    }
+  };
+
+  return (
+    <div className="chat-input-container">
+      <input 
+        placeholder="Send a message to Chatbot."
+        size="30"
+        onChange={saveInputText}
+        value={inputText}
+        className="chat-input"
+        onKeyDown={handleKeyDown}
+        />
+
+      <button 
+        onClick={sendMessage}
+        className="send-button">
+        Send
+      </button>
+    </div>
+  )
+};

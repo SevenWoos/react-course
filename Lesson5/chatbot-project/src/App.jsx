7a4916441febcd8914b96aa694d278581a423200
SevenWoos <swenwu96@gmail.com>
2026-04-11 23:10:31 -0700

    5h. Added new chatbot responses with useEffect.

--- Lesson5/chatbot-project/src/App.jsx
+++ Lesson5/chatbot-project/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Chatbot } from 'supersimpledev';
 import { ChatInput } from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
 import './App.css'
@@ -16,12 +17,21 @@ function App() {
     message: 'can you get me todays date?', 
     sender: 'user', 
     id: crypto.randomUUID()
-  }, 
-{
+  }, {
   message: 'Today is April 11', 
   sender: 'robot', 
   id: crypto.randomUUID()
-}]);
+  }]);
+
+  // useEffect to ADD more Chatbot responses.
+  useEffect(() => {
+    Chatbot.addResponses({
+      'goodbye': 'Goodbye. Have a great day!',
+      'give me a unique id': function() {
+        return `Sure! Here's a unique ID: ${crypto.randomUUID()}`
+      }
+    });
+  });
 
   return (
     <div className="app-container">

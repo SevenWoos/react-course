416450187c42190551d594c2f2c4c100e0aa905c
SevenWoos <swenwu96@gmail.com>
2026-04-11 18:21:01 -0700

    5f. Create login-form React setup using create-vite.

---
+++ Lesson5/login-form/src/App.jsx
@@ -0,0 +1,15 @@
+import LoginForm from './components/LoginForm';
+import './App.css'
+
+// App Component
+function App() {
+  return (
+    <>
+      <p className="title">Hello, welcome to my website</p>
+
+      <LoginForm />
+    </>
+  );
+};
+
+export default App

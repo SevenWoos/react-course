b265eee1bc7ee7ef96bfeb60b3edbce35efb0f32
SevenWoos <swenwu96@gmail.com>
2026-04-11 23:40:10 -0700

    5j. Save messages in localStorage using useEffect.

--- Lesson5/chatbot-project/src/App.jsx
+++ Lesson5/chatbot-project/src/App.jsx
@@ -5,7 +5,7 @@ import ChatMessages from './components/ChatMessages';
 import './App.css'
 
 function App() {
-  const [chatMessages, setChatMessages] = useState([{
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || [{
     message: 'hello chatbot', 
     sender: 'user', 
     id: crypto.randomUUID(), 
@@ -35,7 +35,12 @@ function App() {
         return `Sure! Here's a unique ID: ${crypto.randomUUID()}`
       }
     });
-  });
+  }, []);
+
+  // useEffect to run whenever "chatMessages" changes so we can save chat messages to local Storage.
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages));
+  }, [chatMessages]);
 
   return (
     <div className="app-container">

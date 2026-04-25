e3eaf8f3ee4643e306038d9be90ac0ce9e1496e2
SevenWoos <swenwu96@gmail.com>
2026-04-24 22:26:18 -0700

    8i. Added keyboard events to quantity input.

--- Lesson8/ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ Lesson8/ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -28,6 +28,17 @@ export function CartItemDetails({ cartItem, loadCart }) {
     setQuantity(event.target.value);
   };
 
+  const handleQuantityKeyDown = (event) => {
+    const keyPressed = event.key;
+
+    if (keyPressed === 'Enter') {
+      updateQuantity();
+    } else if (keyPressed === "Escape") {
+      setQuantity(cartItem.quantity);
+      setIsUpdatingQuantity(false);
+    }
+  };
+
   return (
     <>
       <img className="product-image"
@@ -46,7 +57,8 @@ export function CartItemDetails({ cartItem, loadCart }) {
               {isUpdatingQuantity 
                 ? <input type="text" className="quantity-textbox" 
                 value={quantity}
-                onChange={updateQuantityInput} /> 
+                onChange={updateQuantityInput}
+                onKeyDown={handleQuantityKeyDown} /> 
                 : <span className="quantity-label">{cartItem.quantity}</span>
               }
             

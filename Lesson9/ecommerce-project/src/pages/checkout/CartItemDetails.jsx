a7a024c91e144fb6bf0322de5a271bec54bf917f
SevenWoos <swenwu96@gmail.com>
2026-04-27 12:23:38 -0700

    Create quantity state, and save the quantity from the update input.

--- Lesson9/ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ Lesson9/ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -5,6 +5,8 @@ import { formatMoney } from '../../utils/money';
 export function CartItemDetails({ cartItem, loadCart }) {
   const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
 
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
@@ -16,6 +18,10 @@ export function CartItemDetails({ cartItem, loadCart }) {
     } else {
       setIsUpdatingQuantity(true);
     }
+  };
+
+  const updateQuantityInput = (event) => {
+    setQuantity(event.target.value);
   };
 
   return (
@@ -36,7 +42,9 @@ export function CartItemDetails({ cartItem, loadCart }) {
               {
                 isUpdatingQuantity 
                 ? 
-                <input type="text" className="quantity-textbox" />
+                <input type="text" className="quantity-textbox" 
+                value={quantity}
+                onChange={updateQuantityInput} />
                 : 
                 <span className="quantity-label">{cartItem.quantity}</span>
               }

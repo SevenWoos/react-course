a888e94e40d1535c2a5b144f1f4b5ce8e34fbf90
SevenWoos <swenwu96@gmail.com>
2026-04-27 12:16:43 -0700

    Added state to trac if the quantity is being updated and move deleteCartItem feature inside CartItemDetails from OrderSummary.

--- Lesson9/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ Lesson9/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { DeliveryOptions } from './DeliveryOptions';
 import { CartItemDetails } from './CartItemDetails';
 import { DeliveryDate } from './DeliveryDate';
@@ -7,10 +6,6 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
-        const deleteCartItem = async () => {
-          await axios.delete(`/api/cart-items/${cartItem.productId}`);
-          await loadCart();
-        };
 
         return (
           <div key={cartItem.productId}
@@ -20,7 +15,7 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
 
             <div className="cart-item-details-grid">
               
-              <CartItemDetails cartItem={cartItem} deleteCartItem={deleteCartItem} />
+              <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
 
               <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
             </div>

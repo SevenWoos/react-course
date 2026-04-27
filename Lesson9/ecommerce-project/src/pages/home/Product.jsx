4fab395918e14a207d79ef656aded1532c3743bf
SevenWoos <swenwu96@gmail.com>
2026-04-26 23:15:56 -0700

    Make Added message visible when we add an item to the cart.

--- Lesson9/ecommerce-project/src/pages/home/Product.jsx
+++ Lesson9/ecommerce-project/src/pages/home/Product.jsx
@@ -6,6 +6,7 @@ import CheckMarkIcon from '../../assets/images/icons/checkmark.png';
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
   
   const addToCart = async () => {
     await axios.post('/api/cart-items', {
@@ -14,6 +15,12 @@ export function Product({ product, loadCart }) {
       // quantity: quantity
     });
     await loadCart();
+
+    setShowAddedMessage(true);
+
+    setTimeout(() => {
+      setShowAddedMessage(false);
+    }, 2000);
   }
 
   const selectQuantity = (event) => {
@@ -61,7 +68,10 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" 
+        style={{
+          opacity: showAddedMessage ? 1: 0, 
+        }}>
         <img src={CheckMarkIcon} />
         Added
       </div>

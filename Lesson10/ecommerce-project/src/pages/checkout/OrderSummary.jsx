6e3bdb007edeee583b2742fe1c056b8d8e19b05e
SevenWoos <swenwu96@gmail.com>
2026-05-01 13:35:34 -0700

    Create DeliveryDate component and separate from OrderSummary file for lesson 10.

--- Lesson10/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ Lesson10/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,25 +1,17 @@
-import dayjs from 'dayjs';
 import { DeliveryOptions } from './DeliveryOptions';
 import { CartItemDetails } from './CartItemDetails';
+import { DeliveryDate } from './DeliveryDate';
 
 export function OrderSummary({ deliveryOptions, cart }) {
   return (
     <div className="order-summary">
       {/* Delivery Options start off as empty, so we need to check if the delivery options length is greater than 0. */}
-
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
-        // Delivery Date is determined by whichever delivery option is selected. So we need to find it.
-        const selectedDeliveryOption = deliveryOptions
-          .find((deliveryOption) => {
-            return deliveryOption.id === cartItem.deliveryOptionId;
-          });
-
         return (
           <div key={cartItem.productId}
             className="cart-item-container">
-            <div className="delivery-date">
-              Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-            </div>
+            
+            <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
 
             <div className="cart-item-details-grid">
               <CartItemDetails cartItem={cartItem} />

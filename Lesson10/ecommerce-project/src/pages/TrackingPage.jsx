38d68bab974e580442d1ac7fd300795ae1cf9383
SevenWoos <swenwu96@gmail.com>
2026-05-01 14:15:53 -0700

    Update TrackingPage router to include orderId and productId params. Used the useParams hook for lesson 10.

--- Lesson10/ecommerce-project/src/pages/TrackingPage.jsx
+++ Lesson10/ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,8 +1,12 @@
+import { useParams } from 'react-router';
 import { Link } from 'react-router';
 import { Header } from '../components/Header';
+import { OrdersGrid } from './orders/OrdersGrid';
 import './TrackingPage.css';
 
 export function TrackingPage({ cart }) {
+  const { orderId, productId } = useParams();
+
   return (
     <>
       <title>Tracking</title>

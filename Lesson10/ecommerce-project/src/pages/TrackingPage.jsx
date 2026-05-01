77e27d9eab792f07e943fb68c5cfe12c6ecf6850
SevenWoos <swenwu96@gmail.com>
2026-05-01 14:16:53 -0700

    removed unneeded import from TrackingPage.

--- Lesson10/ecommerce-project/src/pages/TrackingPage.jsx
+++ Lesson10/ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router';
 import { Link } from 'react-router';
 import { Header } from '../components/Header';
-import { OrdersGrid } from './orders/OrdersGrid';
 import './TrackingPage.css';
 
 export function TrackingPage({ cart }) {

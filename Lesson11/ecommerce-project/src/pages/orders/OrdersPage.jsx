e44aab5ab1545499d10238f09d80be49e706b0be
SevenWoos <swenwu96@gmail.com>
2026-05-07 11:51:01 -0700

    Separate into smaller components for lesson 11.

--- Lesson11/ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ Lesson11/ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import { useEffect, useState, Fragment } from 'react';
 import { Link } from 'react-router';
-import { Header } from '../components/Header';
-import BuyAgainIcon from '../assets/images/icons/buy-again.png';
-import { formatMoney } from '../utils/money';
+import { Header } from '../../components/Header';
+import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
+import { formatMoney } from '../../utils/money';
 import './Orders.css';
 
 export function OrdersPage({ cart }) {

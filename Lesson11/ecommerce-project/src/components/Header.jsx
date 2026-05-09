2142a8e7b36a7e0344cf9bde9d2a78782c56828c
SevenWoos <swenwu96@gmail.com>
2026-05-08 22:29:57 -0700

    Use useSearchParams to get the search text. When loading the products if search exists, update the URL path to query the products with the search text for lesson 11.

--- Lesson11/ecommerce-project/src/components/Header.jsx
+++ Lesson11/ecommerce-project/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
 import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
@@ -9,7 +9,11 @@ import './Header.css';
 export function Header({ cart }) {
   const navigate = useNavigate();
 
-  const [search, setSearch] = useState('');
+  // const [search, setSearch] = useState('');
+
+  const [searchParams] = useSearchParams();
+  const searchText = searchParams.get('search');
+  const [search, setSearch] = useState(searchText || '');
 
   const updateSearchInput = (event) => {
     setSearch(event.target.value);
